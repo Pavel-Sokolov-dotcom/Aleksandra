f13510896d44566d1348e62df71044862862ee85
Pavel <pashasokolov779@gmail.com>
2026-01-05 13:21:51 +0300

    Добавил комментарии к task_2.py

--- 29dec_11jan/task_2/task_2.py
+++ 29dec_11jan/task_2/task_2.py
@@ -1,98 +1,152 @@
-import json
+import json  # Импортирую json, чтобы конвертировать, в будущем, все данные в json
 
 
-class Student:
-    def __init__(self, name: str, id: int):
-        self.student_name = name
-        self.id = id
-        self.subjects_marks = {}
-    
-    def add_subject(self, subject):
-        if not isinstance(subject, str) or not subject.strip():
-            raise ValueError("Название предмета должно быть не пустым")
-        subject = subject.strip()
-        
-        if subject in self.subjects_marks:
-            raise ValueError(f"Предмет {subject} уже есть")
-        self.subjects_marks[subject] = [] # Добавляю предмет в словарь и значением пустой список для оценок 
-        
-    def remove_subject(self, subject):
-        if not isinstance(subject, str) or not subject.strip():
-            raise ValueError("Название предмета должно быть не пустым")
-        subject = subject.strip()
-        
-        
-        if subject not in self.subjects_marks:
-            raise ValueError(f"Такого предмета {subject} нет, укажите другой предмет")
+class Student:  # Создаю основной класс для этой задачи
+    def __init__(
+        self, name: str, id: int
+    ):  # Экземпляры класса будут содержать имя студента, айди и словарь предметов с оценками
+        self.student_name = (
+            name  # self это ссылка на экземпляр класса, будет создержать имя
+        )
+        self.id = id  # Здесь будет содержаться айди
+        self.subjects_marks = {}  # Здесь будет словарь с предметами и оценками
+
+    def add_subject(self, subject):  # Метод будет добавлять предмет
+        if (
+            not isinstance(subject, str) or not subject.strip()
+        ):  # Здесь проверяю, что то, что передаёт пользователь
+            # является предметом и является строкой и не разделённая строка на список
+            raise ValueError(
+                "Название предмета должно быть не пустым"
+            )  # Если предмет не строка, то вызываю ошибку
+        subject = subject.strip()  # subject ссылается на список предметов
+
+        if (
+            subject in self.subjects_marks
+        ):  # Если предмет уже есть в словаре, то вызовется ошибка
+            raise ValueError(f"Предмет {subject} уже есть")  # Текст ошибки
+        self.subjects_marks[subject] = (
+            []
+        )  # Добавляю предмет в словарь и значением пустой список для оценок
+
+    def remove_subject(self, subject):  # Этот метод будет удалять предмет
+        if (
+            not isinstance(subject, str) or not subject.strip()
+        ):  # Если передана не строка или разделённая строка
+            raise ValueError("Название предмета должно быть не пустым")  # Вызов ошибки
+        subject = subject.strip()  # Предмет ссылается на список из предмета
+
+        if (
+            subject not in self.subjects_marks
+        ):  # Если предмета нет  в словаре Предметов и оценок
+            raise ValueError(
+                f"Такого предмета {subject} нет, укажите другой предмет"
+            )  # То вызываю ошибку
         else:
-            self.subjects_marks.pop(subject) # Удаляю предмет вместе с оценками
-        
-    def add_grade(self, subject: str, mark: int):
-        if not isinstance(subject, str) or not subject.strip():
-            raise ValueError("Название не должно быть пустым")
-        
-        if subject not in self.subjects_marks:
-            raise ValueError(f"Такого предмета {subject} нет")
-        
-        if not isinstance(mark, int):
-            raise TypeError("Оценка должна быть целым числом")
-        if not (1 <= mark <= 5):
-            raise ValueError("Оценка должна быть от 1 до 5 включительно")
-        
-        self.subjects_marks[subject].append(mark)
+            self.subjects_marks.pop(subject)  # Удаляю предмет вместе с оценками
 
+    def add_grade(self, subject: str, mark: int):  # Метод добавляет оценку к предметы
+        if (
+            not isinstance(subject, str) or not subject.strip()
+        ):  # Проверяю, что строка и что не список
+            raise ValueError("Название не должно быть пустым")  # Вызов ошибки
 
-    def get_gpa(self):
-        avg_all_marks = []
-        for mark in self.subjects_marks.values():
-            avg_all_marks.extend(mark)
-        if len(avg_all_marks) == 0:
-            return 0.0
-        
-        return sum(avg_all_marks) / len(avg_all_marks)
-    
-    def __str__(self):
-        if  self.subjects_marks:
+        if (
+            subject not in self.subjects_marks
+        ):  # Если предмета нет в словаре то будет ошибка
+            raise ValueError(f"Такого предмета {subject} нет")  # Сама ошибка
+
+        if not isinstance(mark, int):  # Если оценка не целое число
+            raise TypeError("Оценка должна быть целым числом")  # Ошибка не верного типа
+        if not (
+            1 <= mark <= 5
+        ):  # Здесь проверяю, чтооценка должна быть от 1 до 5 включительно
+            raise ValueError(
+                "Оценка должна быть от 1 до 5 включительно"
+            )  # Вызов ошибки
+
+        self.subjects_marks[subject].append(
+            mark
+        )  # Здесь добавляю оценку в словарь по ключу Предмета
+
+    def get_gpa(self):  # Этот метод получения средней оценки по всем предметам
+        avg_all_marks = []  # Добавляю пустой список
+        for (
+            mark
+        ) in (
+            self.subjects_marks.values()
+        ):  # Прохожу циклом по значениям оценок в словаре
+            avg_all_marks.extend(mark)  # Добавляю в пустой список все оценки
+        if len(avg_all_marks) == 0:  # Если оценок нет то вернётся 0.0
+            return 0.0  # Возврат 0.0
+
+        return sum(avg_all_marks) / len(
+            avg_all_marks
+        )  # Вычисляю среднее значение и возвращаю его
+
+    def __str__(self):  # Метод для отображения читаемой информации
+        if (
+            self.subjects_marks
+        ):  # Если словарь не пустой, то будет буводиться информация о студентах и оценках
             subjects_lines = "\n ".join(
                 f"- {subject}: {marks}"
-                for subject, marks in self.subjects_marks.items()
-            )
+                for subject, marks in self.subjects_marks.items()  # Здесь как лист компехеншн добавление в строку
+            )  # Будет объедлинение в строку Предмет оценка, проходится циклом по всем значениям словаря
             subjects_info = f"Список предметов:\n {subjects_lines}"
         else:
-            subjects_info = "Предметов нет"
+            subjects_info = (
+                "Предметов нет"  # Если словарь пустой, то будет выводиться сообщение
+            )
         return (
             f"Имя студента: {self.student_name} ID ({self.id})\n"
             f"{subjects_info}\n"
             f"Средний бал по всем предметам: {self.get_gpa():.2f}"
-        )
-    
-    def to_dict(self):
+        )  # Возвращаю кортеж строк
+
+    def to_dict(self):  # Метод преобразования в словарь
         return {
             "name": self.student_name,
             "id": self.id,
-            "subjects_marks": self.subjects_marks
-        }
-        
-    @classmethod
-    def from_dict(cls, data): # Это json converter
-        student = cls(data["name"], data["id"])
-        student.subjects_marks = data["subjects_marks"]
-        return student
-    
+            "subjects_marks": self.subjects_marks,
+        }  # Возвращаю словарь студента: имя, id, предметы и оценки
 
-def save_students_to_json(students: list, filename: str):
-    list_of_students = []
-    for student in students:
-        list_of_students.append(student.to_dict())
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(list_of_students, f, ensure_ascii=False, indent=2)
+    @classmethod  # Метод класса, работает с классом
+    def from_dict(cls, data):  # Это json converter принимает данные
+        student = cls(
+            data["name"], data["id"]
+        )  # Переменная сслается на Класс у которого будет имя студента и его id
+        student.subjects_marks = data[
+            "subjects_marks"
+        ]  # Здесь словарю класса присваивается словарь предметов и оценок
+        return student  # Возвращаю объект класса
 
 
-def load_students_from_json(filename: str):
-    students_list = []
-    with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        for item in data:
-            students_list.append(Student.from_dict(item))
-    return students_list
+def save_students_to_json(
+    students: list, filename: str
+):  # Функция сохранения в json формат информации о студентах
+    list_of_students = []  # Объявление пустого списка
+    for student in students:  # Прохожу циклом по переданным студентам
+        list_of_students.append(
+            student.to_dict()
+        )  # Добавляю в пустой список объект студента
+    with open(
+        filename, "w", encoding="utf-8"
+    ) as f:  # С помощью контекстного менеджера открываю файл для чтения
+        json.dump(
+            list_of_students, f, ensure_ascii=False, indent=2
+        )  # Здесь сохраняю всё в json-формат. ensure_ascii=False чтобы Кирилица не экранировалась, отступ в json
 
+
+def load_students_from_json(
+    filename: str,
+):  # Функция чтения данных из json и запись в список
+    students_list = []  # Пустой список, в который будут добавляться словари
+    with open(
+        filename, "r", encoding="utf-8"
+    ) as f:  # Контекстный менеджер открывает файл для чтения
+        data = json.load(f)  # Переменная data ссылается на json, который читает файл
+        for item in data:  # Прохожу циклом по переменной
+            students_list.append(
+                Student.from_dict(item)
+            )  # Добавляю в пустой список данные словаря, которые извлекаются из Класса методом класса
+    return students_list  # Возвращаю список словарей студентов
--- 29dec_11jan/task_2/tests/pytest_task_2.py
+++ 29dec_11jan/task_2/tests/pytest_task_2.py
@@ -4,7 +4,7 @@ from task_2.task_2 import Student, save_students_to_json, load_students_from_jso
 
 def test_create_student():
     Ivan = Student("Ivan", 1)
-    
+
     assert Ivan.student_name == "Ivan"
     assert Ivan.id == 1
     assert Ivan.subjects_marks == {}
@@ -14,29 +14,33 @@ def test_create_student():
 
 def test_add_subject_dublicate():
     Ivan = Student("Ivan", 1)
-    
-    Ivan.add_subject("Математика") # Здесь я добавляю Математику в список предметов
+
+    Ivan.add_subject("Математика")  # Здесь я добавляю Математику в список предметов
     with pytest.raises(ValueError, match="Предмет Математика уже есть"):
-        Ivan.add_subject("Математика") # А здесь проверяю, что дубликат не запишется
-        
+        Ivan.add_subject("Математика")  # А здесь проверяю, что дубликат не запишется
+
 
 def test_add_subject_empty():
     Ivan = Student("Ivan", 1)
-    
+
     with pytest.raises(ValueError, match="Название предмета должно быть не пустым"):
         Ivan.add_subject("")
-        
+
 
 def test_remove_another_subject():
     Sergey = Student("Sergey", 2)
     Sergey.add_subject("Химия")
-    with pytest.raises(ValueError, match="Такого предмета Литература нет, укажите другой предмет"):
+    with pytest.raises(
+        ValueError, match="Такого предмета Литература нет, укажите другой предмет"
+    ):
         Sergey.remove_subject("Литература")
-        
+
 
 def test_remove_nonexistent_subject():
     Sergey = Student("Sergey", 2)
-    with pytest.raises(ValueError, match="Такого предмета Физика нет, укажите другой предмет"):
+    with pytest.raises(
+        ValueError, match="Такого предмета Физика нет, укажите другой предмет"
+    ):
         Sergey.remove_subject("Физика")
 
 
@@ -44,7 +48,7 @@ def test_add_mark_to_subjiect():
     Ekaterina = Student("Екатерина", 3)
     Ekaterina.add_subject("Физика")
     Ekaterina.add_grade("Физика", 5)
-    assert Ekaterina.subjects_marks == {'Физика': [5]}
+    assert Ekaterina.subjects_marks == {"Физика": [5]}
 
 
 def test_add_grade():
@@ -52,13 +56,13 @@ def test_add_grade():
     Elena.add_subject("Труды")
     with pytest.raises(ValueError, match="Оценка должна быть от 1 до 5 включительно"):
         Elena.add_grade("Труды", 6)
-        
+
     with pytest.raises(ValueError, match="Оценка должна быть от 1 до 5 включительно"):
         Elena.add_grade("Труды", 0)
-        
-    with  pytest.raises(TypeError, match="Оценка должна быть целым числом"):
+
+    with pytest.raises(TypeError, match="Оценка должна быть целым числом"):
         Elena.add_grade("Труды", "5")
-    
+
     with pytest.raises(ValueError, match="Такого предмета Химия нет"):
         Elena.add_grade("Химия", 5)
 
@@ -70,7 +74,7 @@ def test_add_grade_some_marks():
     Vlad.add_grade("Физкультура", 5)
     Vlad.add_grade("Физкультура", 3)
     assert Vlad.subjects_marks["Физкультура"] == [4, 5, 3]
-    
+
 
 def test_get_gpa():
     Konstantin = Student("Константин", 6)
@@ -78,20 +82,22 @@ def test_get_gpa():
     Konstantin.add_grade("География", 4)
     Konstantin.add_grade("География", 5)
     assert Konstantin.get_gpa() == 4.5
-    
+
     Konstantin.add_subject("Обществознание")
     Konstantin.add_grade("Обществознание", 3)
     Konstantin.add_grade("Обществознание", 5)
-    assert Konstantin.get_gpa() == 4.25 # По всем оценкам Константина 4 + 5 + 3 + 5 = 17 / 4 = 4.25
-    
+    assert (
+        Konstantin.get_gpa() == 4.25
+    )  # По всем оценкам Константина 4 + 5 + 3 + 5 = 17 / 4 = 4.25
+
     Vladislav = Student("Владислав", 7)
     Vladislav.add_subject("Русский язык")
     assert Vladislav.get_gpa() == 0.0
-    
+
     Vladimir = Student("Владимир", 8)
     assert Vladimir.get_gpa() == 0.0
-    
-    
+
+
 def test_str():
     Olga = Student("Olga", 8)
     Olga.add_subject("Биология")
@@ -104,7 +110,7 @@ def test_str():
     assert "Биология: [5]" in result
     assert "ОБЖ: [4]" in result
     assert "Средний бал по всем предметам: 4.50" in result
-    
+
 
 def test_to_dict():
     Elena = Student("Olga", 8)
@@ -115,7 +121,7 @@ def test_to_dict():
     assert Elena.id == result["id"]
     assert Elena.subjects_marks == result["subjects_marks"]
     assert isinstance(result, dict)
-    
+
 
 def test_from_dict():
     Dmitriy = Student("Дмитрий", 8)
@@ -124,23 +130,24 @@ def test_from_dict():
     result = Dmitriy.to_dict()
     copy_student = Student.from_dict(result)
     assert copy_student.student_name == Dmitriy.student_name
-    assert copy_student.id == Dmitriy.id    
+    assert copy_student.id == Dmitriy.id
     assert copy_student.subjects_marks == Dmitriy.subjects_marks
+
 
 def test_save_load_json_file(tmp_path):
     json_file = tmp_path / "test.json"
     students = []
-    
+
     Oleg = Student("Олег", 9)
     Oleg.add_subject("Информатика")
     Oleg.add_grade("Информатика", 5)
     students.append(Oleg)
-    
+
     Stanislav = Student("Станислав", 10)
     Stanislav.add_subject("Природоведенье")
     Stanislav.add_grade("Природоведенье", 4)
     students.append(Stanislav)
-    
+
     save_students_to_json(students, json_file)
     loaded = load_students_from_json(json_file)
     assert len(loaded) == len(students)
@@ -148,6 +155,3 @@ def test_save_load_json_file(tmp_path):
         assert origin.student_name == load.student_name
         assert origin.id == load.id
         assert origin.subjects_marks == load.subjects_marks
-        
-        
-    
