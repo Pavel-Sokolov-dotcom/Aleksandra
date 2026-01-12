a63589557f94ae7440a1c5b4cb82d080d8f2f6c8
Pavel <pashasokolov779@gmail.com>
2026-01-12 11:14:34 +0300

    Переименовал задачи и добавил комментарии к задаче 2

---
+++ 29dec_11jan/task_1/tests/pytest_task_1.py
@@ -0,0 +1,157 @@
+import pytest
+from task_1.task_1 import Student, save_students_to_json, load_students_from_json
+
+
+def test_create_student():
+    Ivan = Student("Ivan", 1)
+
+    assert Ivan.student_name == "Ivan"
+    assert Ivan.id == 1
+    assert Ivan.subjects_marks == {}
+    Ivan.add_subject("Математика")
+    assert "Математика" in Ivan.subjects_marks
+
+
+def test_add_subject_dublicate():
+    Ivan = Student("Ivan", 1)
+
+    Ivan.add_subject("Математика")  # Здесь я добавляю Математику в список предметов
+    with pytest.raises(ValueError, match="Предмет Математика уже есть"):
+        Ivan.add_subject("Математика")  # А здесь проверяю, что дубликат не запишется
+
+
+def test_add_subject_empty():
+    Ivan = Student("Ivan", 1)
+
+    with pytest.raises(ValueError, match="Название предмета должно быть не пустым"):
+        Ivan.add_subject("")
+
+
+def test_remove_another_subject():
+    Sergey = Student("Sergey", 2)
+    Sergey.add_subject("Химия")
+    with pytest.raises(
+        ValueError, match="Такого предмета Литература нет, укажите другой предмет"
+    ):
+        Sergey.remove_subject("Литература")
+
+
+def test_remove_nonexistent_subject():
+    Sergey = Student("Sergey", 2)
+    with pytest.raises(
+        ValueError, match="Такого предмета Физика нет, укажите другой предмет"
+    ):
+        Sergey.remove_subject("Физика")
+
+
+def test_add_mark_to_subjiect():
+    Ekaterina = Student("Екатерина", 3)
+    Ekaterina.add_subject("Физика")
+    Ekaterina.add_grade("Физика", 5)
+    assert Ekaterina.subjects_marks == {"Физика": [5]}
+
+
+def test_add_grade():
+    Elena = Student("Елена", 4)
+    Elena.add_subject("Труды")
+    with pytest.raises(ValueError, match="Оценка должна быть от 1 до 5 включительно"):
+        Elena.add_grade("Труды", 6)
+
+    with pytest.raises(ValueError, match="Оценка должна быть от 1 до 5 включительно"):
+        Elena.add_grade("Труды", 0)
+
+    with pytest.raises(TypeError, match="Оценка должна быть целым числом"):
+        Elena.add_grade("Труды", "5")
+
+    with pytest.raises(ValueError, match="Такого предмета Химия нет"):
+        Elena.add_grade("Химия", 5)
+
+
+def test_add_grade_some_marks():
+    Vlad = Student("Влад", 5)
+    Vlad.add_subject("Физкультура")
+    Vlad.add_grade("Физкультура", 4)
+    Vlad.add_grade("Физкультура", 5)
+    Vlad.add_grade("Физкультура", 3)
+    assert Vlad.subjects_marks["Физкультура"] == [4, 5, 3]
+
+
+def test_get_gpa():
+    Konstantin = Student("Константин", 6)
+    Konstantin.add_subject("География")
+    Konstantin.add_grade("География", 4)
+    Konstantin.add_grade("География", 5)
+    assert Konstantin.get_gpa() == 4.5
+
+    Konstantin.add_subject("Обществознание")
+    Konstantin.add_grade("Обществознание", 3)
+    Konstantin.add_grade("Обществознание", 5)
+    assert (
+        Konstantin.get_gpa() == 4.25
+    )  # По всем оценкам Константина 4 + 5 + 3 + 5 = 17 / 4 = 4.25
+
+    Vladislav = Student("Владислав", 7)
+    Vladislav.add_subject("Русский язык")
+    assert Vladislav.get_gpa() == 0.0
+
+    Vladimir = Student("Владимир", 8)
+    assert Vladimir.get_gpa() == 0.0
+
+
+def test_str():
+    Olga = Student("Olga", 8)
+    Olga.add_subject("Биология")
+    Olga.add_grade("Биология", 5)
+    Olga.add_subject("ОБЖ")
+    Olga.add_grade("ОБЖ", 4)
+    result = str(Olga)
+    assert "Имя студента: Olga" in result
+    assert "ID (8)" in result
+    assert "Биология: [5]" in result
+    assert "ОБЖ: [4]" in result
+    assert "Средний бал по всем предметам: 4.50" in result
+
+
+def test_to_dict():
+    Elena = Student("Olga", 8)
+    Elena.add_subject("Химия")
+    Elena.add_grade("Химия", 5)
+    result = Elena.to_dict()
+    assert Elena.student_name == result["name"]
+    assert Elena.id == result["id"]
+    assert Elena.subjects_marks == result["subjects_marks"]
+    assert isinstance(result, dict)
+
+
+def test_from_dict():
+    Dmitriy = Student("Дмитрий", 8)
+    Dmitriy.add_subject("Химия")
+    Dmitriy.add_grade("Химия", 5)
+    result = Dmitriy.to_dict()
+    copy_student = Student.from_dict(result)
+    assert copy_student.student_name == Dmitriy.student_name
+    assert copy_student.id == Dmitriy.id
+    assert copy_student.subjects_marks == Dmitriy.subjects_marks
+
+
+def test_save_load_json_file(tmp_path):
+    json_file = tmp_path / "test.json"
+    students = []
+
+    Oleg = Student("Олег", 9)
+    Oleg.add_subject("Информатика")
+    Oleg.add_grade("Информатика", 5)
+    students.append(Oleg)
+
+    Stanislav = Student("Станислав", 10)
+    Stanislav.add_subject("Природоведенье")
+    Stanislav.add_grade("Природоведенье", 4)
+    students.append(Stanislav)
+
+    save_students_to_json(students, json_file)
+    loaded = load_students_from_json(json_file)
+    assert len(loaded) == len(students)
+    for origin, load in zip(students, loaded):
+        assert origin.student_name == load.student_name
+        assert origin.id == load.id
+        assert origin.subjects_marks == load.subjects_marks
--- 29dec_11jan/task_2/task_2.py
+++ 29dec_11jan/task_2/task_2.py
@@ -1,152 +1,162 @@
-import json  # Импортирую json, чтобы конвертировать, в будущем, все данные в json
+import logging  # импортирую модуль логирования
 
 
-class Student:  # Создаю основной класс для этой задачи
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)  # Настраиваю логирование, уровень INFO и формат: время, имя уровня и сообщение
+
+
+class HTTPRequest:  # Создаю  класс для  запросов
     def __init__(
-        self, name: str, id: int
-    ):  # Экземпляры класса будут содержать имя студента, айди и словарь предметов с оценками
-        self.student_name = (
-            name  # self это ссылка на экземпляр класса, будет создержать имя
-        )
-        self.id = id  # Здесь будет содержаться айди
-        self.subjects_marks = {}  # Здесь будет словарь с предметами и оценками
-
-    def add_subject(self, subject):  # Метод будет добавлять предмет
-        if (
-            not isinstance(subject, str) or not subject.strip()
-        ):  # Здесь проверяю, что то, что передаёт пользователь
-            # является предметом и является строкой и не разделённая строка на список
-            raise ValueError(
-                "Название предмета должно быть не пустым"
-            )  # Если предмет не строка, то вызываю ошибку
-        subject = subject.strip()  # subject ссылается на список предметов
-
-        if (
-            subject in self.subjects_marks
-        ):  # Если предмет уже есть в словаре, то вызовется ошибка
-            raise ValueError(f"Предмет {subject} уже есть")  # Текст ошибки
-        self.subjects_marks[subject] = (
-            []
-        )  # Добавляю предмет в словарь и значением пустой список для оценок
-
-    def remove_subject(self, subject):  # Этот метод будет удалять предмет
-        if (
-            not isinstance(subject, str) or not subject.strip()
-        ):  # Если передана не строка или разделённая строка
-            raise ValueError("Название предмета должно быть не пустым")  # Вызов ошибки
-        subject = subject.strip()  # Предмет ссылается на список из предмета
-
-        if (
-            subject not in self.subjects_marks
-        ):  # Если предмета нет  в словаре Предметов и оценок
-            raise ValueError(
-                f"Такого предмета {subject} нет, укажите другой предмет"
-            )  # То вызываю ошибку
+        self, url: str, method: str, headers=None, body: str = None, timeout=30
+    ):  # В экземпляр класса нужно будет передать url, method, headers, body, timeout
+        self.url = url  # Это адрес в формате строки
+        self.method = method  # Это метод GET, POST  и  т.д.
+        self.headers = headers if headers is not None else {}  # Заголовки в словарь
+        if body is None:  # Условие
+            self.body = ""  # Если боди None, то будет равен пустой строке
+        elif isinstance(body, str):
+            self.body = body  # Если body является аргументом str, то будет равен тому, что передаст пользователь
         else:
-            self.subjects_marks.pop(subject)  # Удаляю предмет вместе с оценками
+            raise TypeError("Body должен быть или строкой или None")  # Вызов ошибки
+        self.timeout = timeout  # Передаётся таймаут
 
-    def add_grade(self, subject: str, mark: int):  # Метод добавляет оценку к предметы
-        if (
-            not isinstance(subject, str) or not subject.strip()
-        ):  # Проверяю, что строка и что не список
-            raise ValueError("Название не должно быть пустым")  # Вызов ошибки
 
-        if (
-            subject not in self.subjects_marks
-        ):  # Если предмета нет в словаре то будет ошибка
-            raise ValueError(f"Такого предмета {subject} нет")  # Сама ошибка
+class HTTPRequestBuilder:  # Класс создание запроса
+    def __init__(self):
+        self._url = None
+        self._method = None
+        self._headers = {}
+        self._body = None
+        self._timeout = 30
 
-        if not isinstance(mark, int):  # Если оценка не целое число
-            raise TypeError("Оценка должна быть целым числом")  # Ошибка не верного типа
-        if not (
-            1 <= mark <= 5
-        ):  # Здесь проверяю, чтооценка должна быть от 1 до 5 включительно
+    def set_url(self, url: str):  # Метод установки  url
+        if not isinstance(url, str):  # Если не строка
+            raise TypeError("URL должен быть строкой")  # Вызов ошибки
+        if not url.startswith(
+            ("http://", "https://")
+        ):  # Если начинается не с http://, https://
             raise ValueError(
-                "Оценка должна быть от 1 до 5 включительно"
-            )  # Вызов ошибки
+                "URL должен начинаться с http:// или с https://"
+            )  # Вызов  ошибки
+        self._url = url  # Атрибут экземпляра ссылается на переданный адрес
+        return self  # Возвращаю сам экземпляр
 
-        self.subjects_marks[subject].append(
-            mark
-        )  # Здесь добавляю оценку в словарь по ключу Предмета
+    def set_method(self, method: str):  # Установка метода
+        if method.upper() not in {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "PATCH",
+        }:  # Если нет во множестве вариантов
+            raise ValueError(
+                "Вы указали не метод HTTP, нужно указать один из: POST, GET, PUT, DELETE, PATCH"
+            )  #  Вызов  ошибки
+        self._method = method.upper()  # Ссылается  на один из прописанных методов
+        return self  # Возвращаю сам экземпляр
 
-    def get_gpa(self):  # Этот метод получения средней оценки по всем предметам
-        avg_all_marks = []  # Добавляю пустой список
-        for (
-            mark
-        ) in (
-            self.subjects_marks.values()
-        ):  # Прохожу циклом по значениям оценок в словаре
-            avg_all_marks.extend(mark)  # Добавляю в пустой список все оценки
-        if len(avg_all_marks) == 0:  # Если оценок нет то вернётся 0.0
-            return 0.0  # Возврат 0.0
+    def add_header(self, key: str, value: str): # Это метод добавления заголовка с ключом и значением для словаря, как в json
+        if isinstance(key, str) and isinstance(value, str):  # Если переданные значения строки, то добавятся
+            self._headers[key] = value # Сам добавление значений
+        return self # Возвращаю экземпляр класса с новыми данными
 
-        return sum(avg_all_marks) / len(
-            avg_all_marks
-        )  # Вычисляю среднее значение и возвращаю его
+    def set_body(self, body: str): # Метод установки "тела"
+        if body is not None and not isinstance(body, str): # Проверка, что тело не None и строка
+            raise TypeError("Должна быть передана строка") # Вызов ошибки
+        self._body = body # Установка значения тела
+        return self # Возвращаю экземпляр класса
 
-    def __str__(self):  # Метод для отображения читаемой информации
-        if (
-            self.subjects_marks
-        ):  # Если словарь не пустой, то будет буводиться информация о студентах и оценках
-            subjects_lines = "\n ".join(
-                f"- {subject}: {marks}"
-                for subject, marks in self.subjects_marks.items()  # Здесь как лист компехеншн добавление в строку
-            )  # Будет объедлинение в строку Предмет оценка, проходится циклом по всем значениям словаря
-            subjects_info = f"Список предметов:\n {subjects_lines}"
-        else:
-            subjects_info = (
-                "Предметов нет"  # Если словарь пустой, то будет выводиться сообщение
+    def set_timeout(self, timeout: int | float): # Метод установки перерыва
+        if isinstance(timeout, (int, float)) and timeout > 0: # Проверяю, что передано число или число с плавающей точкой
+            self._timeout = timeout # Устанавливаю значение таймаута
+        return self # Возварщаю экземпляр класса
+
+    def build(self): # Метод сборки
+        if self._url is None or self._method is None: # Проверяю если пустые значения, то будет ошика
+            raise ValueError("url и method должны быть пррописаны") # Вызов ошибки
+        return HTTPRequest(
+            url=self._url,
+            method=self._method,
+            headers=self._headers,
+            body=self._body,
+            timeout=self._timeout,
+        ) # Возвращаю запрос со всеми переданными значениями
+
+
+class ValidationDecorator:
+    def __init__(self, builder):
+        self._builder = builder
+
+    def set_url(self, url: str):
+        if not isinstance(url, str):
+            raise TypeError("Должна быть передана строка")
+        if not url.startswith(("http://", "https://")):
+            raise ValueError("URL должен начинаться с http:// или с https://")
+        return self._builder.set_url(url)
+
+    def set_method(self, method: str):
+        if method.upper() not in {"GET", "POST", "PUT", "DELETE", "PATCH"}:
+            raise ValueError(
+                "Вы указали не метод HTTP, нужно указать один из: POST, GET, PUT, DELETE, PATCH"
             )
-        return (
-            f"Имя студента: {self.student_name} ID ({self.id})\n"
-            f"{subjects_info}\n"
-            f"Средний бал по всем предметам: {self.get_gpa():.2f}"
-        )  # Возвращаю кортеж строк
+        return self._builder.set_method(method)
 
-    def to_dict(self):  # Метод преобразования в словарь
-        return {
-            "name": self.student_name,
-            "id": self.id,
-            "subjects_marks": self.subjects_marks,
-        }  # Возвращаю словарь студента: имя, id, предметы и оценки
+    def add_header(self, key: str, value: str):
+        if not isinstance(key, str) or not isinstance(value, str):
+            raise TypeError("Ключ и значение должны быть строкой")
+        return self._builder.add_header(key, value)
 
-    @classmethod  # Метод класса, работает с классом
-    def from_dict(cls, data):  # Это json converter принимает данные
-        student = cls(
-            data["name"], data["id"]
-        )  # Переменная сслается на Класс у которого будет имя студента и его id
-        student.subjects_marks = data[
-            "subjects_marks"
-        ]  # Здесь словарю класса присваивается словарь предметов и оценок
-        return student  # Возвращаю объект класса
+    def set_body(self, body: str | None):
+        if body is not None and not isinstance(body, str):
+            raise ValueError("Должна быть передана строка")
+        return self._builder.set_body(body)
+
+    def set_timeout(self, timeout: int | float):
+        return self._builder.set_timeout(timeout)
+
+    def build(self):
+        return self._builder.build()
 
 
-def save_students_to_json(
-    students: list, filename: str
-):  # Функция сохранения в json формат информации о студентах
-    list_of_students = []  # Объявление пустого списка
-    for student in students:  # Прохожу циклом по переданным студентам
-        list_of_students.append(
-            student.to_dict()
-        )  # Добавляю в пустой список объект студента
-    with open(
-        filename, "w", encoding="utf-8"
-    ) as f:  # С помощью контекстного менеджера открываю файл для чтения
-        json.dump(
-            list_of_students, f, ensure_ascii=False, indent=2
-        )  # Здесь сохраняю всё в json-формат. ensure_ascii=False чтобы Кирилица не экранировалась, отступ в json
+class LoggingDecorator:
+    def __init__(self, builder):
+        self._builder = builder
+
+    def set_url(self, url: str):
+        logging.info(f"Настройки url: {url}")
+        return self._builder.set_url(url)
+
+    def set_method(self, method: str):
+        logging.info(f"Передан метод {method}")
+        return self._builder.set_method(method)
+
+    def add_header(self, key: str, value: str):
+        logging.info(f"Переданы значения: ключ {key}, значение {value}")
+        return self._builder.add_header(key, value)
+
+    def set_body(self, body):
+        logging.info(f"Передано {body}")
+        return self._builder.set_body(body)
+
+    def set_timeout(self, timeout):
+        logging.info(f"Установлен таймаут {timeout} секунд")
+        return self._builder.set_timeout(timeout)
+
+    def build(self):
+        return self._builder.build()
 
 
-def load_students_from_json(
-    filename: str,
-):  # Функция чтения данных из json и запись в список
-    students_list = []  # Пустой список, в который будут добавляться словари
-    with open(
-        filename, "r", encoding="utf-8"
-    ) as f:  # Контекстный менеджер открывает файл для чтения
-        data = json.load(f)  # Переменная data ссылается на json, который читает файл
-        for item in data:  # Прохожу циклом по переменной
-            students_list.append(
-                Student.from_dict(item)
-            )  # Добавляю в пустой список данные словаря, которые извлекаются из Класса методом класса
-    return students_list  # Возвращаю список словарей студентов
+builder = HTTPRequestBuilder()
+
+decorator_builder = LoggingDecorator(ValidationDecorator(builder=builder))
+
+
+request = (
+    decorator_builder.set_url("https://api.ipify.org/?format=json")
+    .set_method("GET")
+    .add_header("My_IP", "json")
+    .set_body("{'name': 'Ivan'}")
+    .set_timeout(10)
+    .build()
+)
