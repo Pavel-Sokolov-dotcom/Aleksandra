f44ef9c72194ef3ee9f6148bac007c5bd7f97ffa
Pavel <pashasokolov779@gmail.com>
2026-01-16 20:07:34 +0300

    Вторая задача из списка 12-25 января

---
+++ 12jan_25jan/task_2/task_2.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from collections import defaultdict
+
+
+FILE_PATH = Path(__file__).parent / "access.log"
+
+
+def reading_loog(filename):
+    method_count = {
+        "GET": 0,
+        "POST": 0,
+        "PUT": 0,
+        "DELETE": 0,
+        "PATCH": 0,
+        "HEAD": 0,
+        "OPTIONS": 0,
+        "CONNECT": 0,
+        "TRACE": 0,
+    }  # Да знаю, много, но что поделать
+    ip_adress = defaultdict(int)
+    responce_time = []
+    errors_codes = defaultdict(int)
+
+    with open(filename, "r", encoding="utf-8") as file: # TODO Написать коменты к каждой строчке
+        for method in file.readlines():
+            m = method.split()
+            method_count[m[4].replace('"', "")] += 1
+            ip_adress[m[0]] += 1
+            if m[7].startswith(("4", "5")):
+                errors_codes[m[7]] += 1
+            responce_time.append(int(m[8]))
+
+    sorted_ip_adress = sorted(
+        ip_adress.items(), key=lambda x: x[1], reverse=True
+    )  #  * Отортировал по убыванию и буду брать только первые 10
+    top_ip_adress = "\n\t".join(
+        [f"ip {key}: кол-во раз {value}" for key, value in sorted_ip_adress[:10]]
+    )
+
+    if len(responce_time) == 0:  # Если список пустой, будет ошибка
+        raise ZeroDivisionError("Список пустой, на ноль делить нельзя")
+    else:
+        average_responce_time = sum(responce_time) / len(responce_time)
+
+    final_method_count = "\n\t".join(
+        [f"{key}: {value}" for key, value in method_count.items()]
+    )
+    final_errors_code = "\n\t".join(
+        [
+            f"статус {key}: количество ошибок {value}"
+            for key, value in errors_codes.items()
+        ]
+    )
+
+    return f"""
+    Количество запросов:\n\t{final_method_count}\n
+    ТОП IP-адресов:\n\t{top_ip_adress}\n
+    Список ошибок:\n\t{final_errors_code}\n
+    Среднее время ответа = {average_responce_time}\n
+    """
+
+
+print(reading_loog(FILE_PATH))
--- 29dec_11jan/task_2/task_2.py
+++ 29dec_11jan/task_2/task_2.py
@@ -56,94 +56,135 @@ class HTTPRequestBuilder:  # Класс создание запроса
         self._method = method.upper()  # Ссылается  на один из прописанных методов
         return self  # Возвращаю сам экземпляр
 
-    def add_header(self, key: str, value: str): # Это метод добавления заголовка с ключом и значением для словаря, как в json
-        if isinstance(key, str) and isinstance(value, str):  # Если переданные значения строки, то добавятся
-            self._headers[key] = value # Сам добавление значений
-        return self # Возвращаю экземпляр класса с новыми данными
+    def add_header(
+        self, key: str, value: str
+    ):  # Это метод добавления заголовка с ключом и значением для словаря, как в json
+        if isinstance(key, str) and isinstance(
+            value, str
+        ):  # Если переданные значения строки, то добавятся
+            self._headers[key] = value  # Сам добавление значений
+        return self  # Возвращаю экземпляр класса с новыми данными
 
-    def set_body(self, body: str): # Метод установки "тела"
-        if body is not None and not isinstance(body, str): # Проверка, что тело не None и строка
-            raise TypeError("Должна быть передана строка") # Вызов ошибки
-        self._body = body # Установка значения тела
-        return self # Возвращаю экземпляр класса
+    def set_body(self, body: str):  # Метод установки "тела"
+        if body is not None and not isinstance(
+            body, str
+        ):  # Проверка, что тело не None и строка
+            raise TypeError("Должна быть передана строка")  # Вызов ошибки
+        self._body = body  # Установка значения тела
+        return self  # Возвращаю экземпляр класса
 
-    def set_timeout(self, timeout: int | float): # Метод установки перерыва
-        if isinstance(timeout, (int, float)) and timeout > 0: # Проверяю, что передано число или число с плавающей точкой
-            self._timeout = timeout # Устанавливаю значение таймаута
-        return self # Возварщаю экземпляр класса
+    def set_timeout(self, timeout: int | float):  # Метод установки перерыва
+        if (
+            isinstance(timeout, (int, float)) and timeout > 0
+        ):  # Проверяю, что передано число или число с плавающей точкой
+            self._timeout = timeout  # Устанавливаю значение таймаута
+        return self  # Возварщаю экземпляр класса
 
-    def build(self): # Метод сборки
-        if self._url is None or self._method is None: # Проверяю если пустые значения, то будет ошика
-            raise ValueError("url и method должны быть пррописаны") # Вызов ошибки
+    def build(self):  # Метод сборки
+        if (
+            self._url is None or self._method is None
+        ):  # Проверяю если пустые значения, то будет ошика
+            raise ValueError("url и method должны быть пррописаны")  # Вызов ошибки
         return HTTPRequest(
             url=self._url,
             method=self._method,
             headers=self._headers,
             body=self._body,
             timeout=self._timeout,
-        ) # Возвращаю запрос со всеми переданными значениями
+        )  # Возвращаю запрос со всеми переданными значениями
 
 
-class ValidationDecorator: # Это  класс  валидатор, ещё это класс. Он проверяет валидность переданных данных
+class ValidationDecorator:  # Это  класс  валидатор, ещё это класс. Он проверяет валидность переданных данных
     def __init__(self, builder):
-        self._builder = builder # Это будет сборщик в экземпляре класса
+        self._builder = builder  # Это будет сборщик в экземпляре класса
 
-    def set_url(self, url: str): # Метод для проверки адреса
-        if not isinstance(url, str): # Если не строка, то будет ошибка
-            raise TypeError("Должна быть передана строка") # Вызов ошибки
-        if not url.startswith(("http://", "https://")): # Если начинается не с того, что ожидается
-            raise ValueError("URL должен начинаться с http:// или с https://") # Вызов ошибки
-        return self._builder.set_url(url) # Возвращаю через декоратор адрес сайта
+    def set_url(self, url: str):  # Метод для проверки адреса
+        if not isinstance(url, str):  # Если не строка, то будет ошибка
+            raise TypeError("Должна быть передана строка")  # Вызов ошибки
+        if not url.startswith(
+            ("http://", "https://")
+        ):  # Если начинается не с того, что ожидается
+            raise ValueError(
+                "URL должен начинаться с http:// или с https://"
+            )  # Вызов ошибки
+        return self._builder.set_url(url)  # Возвращаю через декоратор адрес сайта
 
-    def set_method(self, method: str): # Метод декоратора для проверки HTTP метода
-        if method.upper() not in {"GET", "POST", "PUT", "DELETE", "PATCH"}: # Если нет во множестве нужного методы будет ошибка
+    def set_method(self, method: str):  # Метод декоратора для проверки HTTP метода
+        if method.upper() not in {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "PATCH",
+        }:  # Если нет во множестве нужного методы будет ошибка
             raise ValueError(
                 "Вы указали не метод HTTP, нужно указать один из: POST, GET, PUT, DELETE, PATCH"
-            ) # Вызов ошибки
-        return self._builder.set_method(method) # Возвращаю метод через декоратор
+            )  # Вызов ошибки
+        return self._builder.set_method(method)  # Возвращаю метод через декоратор
 
-    def add_header(self, key: str, value: str): # Добавление хедера
-        if not isinstance(key, str) or not isinstance(value, str): # Проверка что переданные данные строкового типа
-            raise TypeError("Ключ и значение должны быть строкой") # Вызов ошибки
-        return self._builder.add_header(key, value) # Возвращаю хедер через  декоратор
+    def add_header(self, key: str, value: str):  # Добавление хедера
+        if not isinstance(key, str) or not isinstance(
+            value, str
+        ):  # Проверка что переданные данные строкового типа
+            raise TypeError("Ключ и значение должны быть строкой")  # Вызов ошибки
+        return self._builder.add_header(key, value)  # Возвращаю хедер через  декоратор
 
-    def set_body(self, body: str | None): # Метод добавления боди
-        if body is not None and not isinstance(body, str): # Проверяю, что не пусто и передана именно строка
-            raise ValueError("Должна быть передана строка") # Вызов ошибки
-        return self._builder.set_body(body) # Возвращаю значение боди через декоратор валидатоор
+    def set_body(self, body: str | None):  # Метод добавления боди
+        if body is not None and not isinstance(
+            body, str
+        ):  # Проверяю, что не пусто и передана именно строка
+            raise ValueError("Должна быть передана строка")  # Вызов ошибки
+        return self._builder.set_body(
+            body
+        )  # Возвращаю значение боди через декоратор валидатоор
 
-    def set_timeout(self, timeout: int | float): # Метод установки таймаута. Принимает или int или float
-        if not isinstance(timeout, (int, float)): # Условие что или int или float
-            raise TypeError("Тайм аут должен быть или целым числом или числом с плавающей точкой") # или int или float
-        return self._builder.set_timeout(timeout) # Возвращаю значение боди через декоратор валидатор
+    def set_timeout(
+        self, timeout: int | float
+    ):  # Метод установки таймаута. Принимает или int или float
+        if not isinstance(timeout, (int, float)):  # Условие что или int или float
+            raise TypeError(
+                "Тайм аут должен быть или целым числом или числом с плавающей точкой"
+            )  # или int или float
+        return self._builder.set_timeout(
+            timeout
+        )  # Возвращаю значение боди через декоратор валидатор
 
-    def build(self): # Метод сбора всех данных
-        return self._builder.build() # Возвращаю сборщик
+    def build(self):  # Метод сбора всех данных
+        return self._builder.build()  # Возвращаю сборщик
+
 
 # Декоратор логирования во всех методах берёт данные из класса валидатора
-class LoggingDecorator: # Класс для логирования. Сделал не через print
-    def __init__(self, builder): # Инициализация сборщика экземпляра класса
-        self._builder = builder # Ссылка на переданный сборщик
+class LoggingDecorator:  # Класс для логирования. Сделал не через print
+    def __init__(self, builder):  # Инициализация сборщика экземпляра класса
+        self._builder = builder  # Ссылка на переданный сборщик
 
-    def set_url(self, url: str): # Метод добавления адреса сайта 
-        logging.info(f"Настройки url: {url}") # Лог об  адресе, правильно ли его передали
-        return self._builder.set_url(url) # Возарщаю сборщик
+    def set_url(self, url: str):  # Метод добавления адреса сайта
+        logging.info(
+            f"Настройки url: {url}"
+        )  # Лог об  адресе, правильно ли его передали
+        return self._builder.set_url(url)  # Возарщаю сборщик
 
-    def set_method(self, method: str): # Метод установки метода
-        logging.info(f"Передан метод {method}") # Лог об успешной или не успешной передачи метода
-        return self._builder.set_method(method) # Возвращаю метод 
+    def set_method(self, method: str):  # Метод установки метода
+        logging.info(
+            f"Передан метод {method}"
+        )  # Лог об успешной или не успешной передачи метода
+        return self._builder.set_method(method)  # Возвращаю метод
 
-    def add_header(self, key: str, value: str): # Метод добавления хедера
-        logging.info(f"Переданы значения: ключ {key}, значение {value}") # Выведет параметры
-        return self._builder.add_header(key, value) # Возвращаю переданные параметры
+    def add_header(self, key: str, value: str):  # Метод добавления хедера
+        logging.info(
+            f"Переданы значения: ключ {key}, значение {value}"
+        )  # Выведет параметры
+        return self._builder.add_header(key, value)  # Возвращаю переданные параметры
 
-    def set_body(self, body): # Метод установки тела запроса
-        logging.info(f"Передано {body}") # Лог об body
-        return self._builder.set_body(body) # Возвращаю установленное тело запроса
+    def set_body(self, body):  # Метод установки тела запроса
+        logging.info(f"Передано {body}")  # Лог об body
+        return self._builder.set_body(body)  # Возвращаю установленное тело запроса
 
-    def set_timeout(self, timeout): # Метод установки таймаута
-        logging.info(f"Установлен таймаут {timeout} секунд") # Лог об установлении таймаута
-        return self._builder.set_timeout(timeout) # Возвращаю значение таймаута
+    def set_timeout(self, timeout):  # Метод установки таймаута
+        logging.info(
+            f"Установлен таймаут {timeout} секунд"
+        )  # Лог об установлении таймаута
+        return self._builder.set_timeout(timeout)  # Возвращаю значение таймаута
 
-    def build(self): # Собираю весь запрос
-        return self._builder.build() # Возвращаю запрос целиком
+    def build(self):  # Собираю весь запрос
+        return self._builder.build()  # Возвращаю запрос целиком
